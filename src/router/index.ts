af5490168553a84fc419b2f91b30ef1e3bab5040
nik-kita <nik-kita.deno.dev>
2024-03-05 00:18:33 +0100

    CalculatorView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CalculatorView from '../views/CalculatorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/calculator',
+      name: 'calculator',
+      component: CalculatorView
     }
   ]
 })
