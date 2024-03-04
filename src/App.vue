4e79792bb74a31b0852686b345680a927c11c512
nik-kita <nik-kita.deno.dev>
2024-03-04 19:14:52 +0100

    declare RadioGroup, @pickedValue from child's watchEffect()

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import RadioGroup from './lib/RadioGroup.vue';
+
+const title = ref('');
+
+function updateTitle(fresh: string) {
+  console.log(fresh);
+  title.value = fresh;
+}
 
 </script>
 
@@ -7,12 +15,46 @@ import RadioGroup from './lib/RadioGroup.vue';
 
 <template>
   <div class="app root">
-    <RadioGroup />
+    <div class="app header">{{ title }}!</div>
+    <div class="app content">
+      <RadioGroup @picked-value="updateTitle" :radios="[
+      {
+        value: 'hello world',
+      },
+      {
+        value: 'ok, google',
+      },
+      {
+        value: 'foo bar baz'
+      }
+    ]" />
+    </div>
   </div>
 </template>
 
 <style>
 .app.root {
   height: 100%;
+  display: grid;
+  grid-template:
+    "h h h h h"
+    "s c c c c"
+    "s c c c c"
+    "s c c c c"
+    "f f f f f"
+    "f f f f f"
+}
+
+.app.header {
+  grid-area: h;
+  display: flex;
+  align-items: center;
+  background-color: black;
+  color: white;
+}
+
+.app.content {
+  padding: 2rem;
+  grid-area: c;
 }
 </style>
--- src/lib/RadioGroup.vue
+++ src/lib/RadioGroup.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts">
+import { ref, watchEffect } from 'vue';
+
+
+const props = defineProps<{
+  radios: {
+    value: string,
+    label?: string,
+  }[],
+  value?: string,
+}>();
+const groupValue = ref(props.value);
+const emit = defineEmits<{
+  pickedValue: [value: string]
+}>();
+
+watchEffect(() => {
+  emit('pickedValue', groupValue.value ?? '');
+});
 
 </script>
 
 <template>
-  RadioGroup
+  <div class="radio-group">
+    <div class="radio" v-for="{ value, label } of props.radios" :key="value">
+      <label :for="value">{{ label || value }}</label>
+      <input class="radio-input" type="radio" :value="value" v-model="groupValue" />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.radio:hover {
+  background-color: lightcoral;
+}
+</style>
