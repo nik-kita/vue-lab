3e07c57be87504b384d1b6552803996233359058
nik-kita <nik-kita.deno.dev>
2024-03-04 22:58:11 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import RadioGroup from './RadioGroup.vue';
 
-
+const radioGroupItems = [
+  { value: 'hello' },
+  { value: 'world' },
+];
+const title = ref('');
 
 </script>
 
@@ -10,7 +15,12 @@ import RadioGroup from './RadioGroup.vue';
 <template>
   <div class="app root">
     <div class="app pink content">
-      <RadioGroup />
+      <RadioGroup :items="radioGroupItems" v-model="title" />
+    </div>
+    <div class="app header">
+      <h1>
+        {{ title }}
+      </h1>
     </div>
   </div>
 </template>
@@ -20,19 +30,28 @@ import RadioGroup from './RadioGroup.vue';
   height: 100%;
   display: grid;
   grid-template:
-    "h h h h h h h"
-    "r r c c c c l"
-    "r r c c c c l"
-    "r r c c c c l"
-    "r r c c c c l"
-    "f f f f f f f"
-    "f f f f f f f"
+    "h h h h h h h" 1fr
+    "r r c c c c l" 1fr
+    "r r c c c c l" 1fr
+    "r r c c c c l" 1fr
+    "r r c c c c l" 1fr
+    "f f f f f f f" 1fr
+    "f f f f f f f" 1fr
+    / 1fr 1fr 1fr 1fr 1fr 1fr 1fr
   ;
+}
+
+.app.header {
+  grid-area: h;
+  display: flex;
+  justify-content: center;
+  place-items: center;
 }
 
 .app.content {
   grid-area: c;
   display: flex;
+  flex-flow: column nowrap;
   justify-content: center;
   place-items: center;
   text-align: center;
--- src/RadioGroup.vue
+++ src/RadioGroup.vue
@@ -1,6 +1,29 @@
 <script setup lang="ts">
+/* eslint-disable vue/no-mutating-props */
+const props = defineProps<{
+  items: {
+    value: string,
+    label?: string,
+  }[],
+}>();
+const picked = defineModel()
 </script>
 
 <template>
-  RadioGroup
+  <template v-for="{ value, label } of props.items" :key="label">
+    <label class="radio-group item">
+      {{ label ?? value }}
+      <input type="radio" :value="value" v-model="picked" />
+    </label>
+  </template>
 </template>
+
+<style>
+.radio-group.item {
+  width: 100%;
+}
+.radio-group.item:hover {
+  background-color: rgba(20, 20, 20, 0.5);
+  color: white;
+}
+</style>
