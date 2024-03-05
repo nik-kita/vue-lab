de8a922daae5759167ab8871adb739ac378cd5fa
nik-kita <nik-kita.deno.dev>
2024-03-05 02:34:09 +0100

    disable operation for first input

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -2,14 +2,19 @@
 import { ref } from 'vue'
 import CalculatorNumInput from '../components/CalculatorNumInput.vue'
 
+const history = [] as string[]
+const isNumAllowed = ref(true)
+const isOperationAllowed = ref(false)
 const monitorOutput = ref('0')
 function processNumClick(num: number) {
+  isOperationAllowed.value = true
   const firstPart = monitorOutput.value === '0' ? '' : monitorOutput.value
   const lastChar = monitorOutput.value.at(-1)
 
   monitorOutput.value = firstPart + (Number.isNaN(lastChar) && lastChar !== '.' ? ` ${num}` : num)
 }
 function processC() {
+  isNumAllowed.value = true
   monitorOutput.value = '0'
 }
 function processBackspace() {
@@ -25,6 +30,7 @@ function processBackspace() {
   <main class="calculator root">
     <textarea class="calculator monitor" type="text" disabled v-model="monitorOutput"></textarea>
     <CalculatorNumInput
+      :disabled="!isNumAllowed"
       v-for="(_, i) in 10"
       :key="i"
       :value="i"
@@ -34,17 +40,17 @@ function processBackspace() {
     />
     <button @click="processBackspace" class="calculator backspace">Backspace</button>
     <button @click="processC" value="C" class="calculator c">C</button>
-    <button class="calculator divide">/</button>
-    <button class="calculator multiply">x</button>
-    <button class="calculator minus">-</button>
-    <button class="calculator plus">+</button>
+    <button :disabled="!isOperationAllowed" class="calculator divide">/</button>
+    <button :disabled="!isOperationAllowed" class="calculator multiply">x</button>
+    <button :disabled="!isOperationAllowed" class="calculator minus">-</button>
+    <button :disabled="!isOperationAllowed" class="calculator plus">+</button>
     <button
       @click="() => (monitorOutput += monitorOutput.at(-1) === '.' ? '' : '.')"
       class="calculator dot"
     >
       .
     </button>
-    <button class="calculator equals">=</button>
+    <button :disabled="!isOperationAllowed" class="calculator equals">=</button>
   </main>
 </template>
 
