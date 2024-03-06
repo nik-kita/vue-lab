c3e09ced7362efac44ed654177fdf61a114f9df4
nik-kita <nik-kita.deno.dev>
2024-03-07 00:53:12 +0100

    little prgress

--- src/components/textarea/for-grid-template/MainComponent.vue
+++ src/components/textarea/for-grid-template/MainComponent.vue
@@ -1,17 +1,16 @@
-<script
-  setup
-  lang="ts"
->
+<script setup lang="ts">
 import { computed, ref } from 'vue'
 import { gen_alone_enter_handler } from './gen-alone-enter-handler'
 
-const MIN_TEXTAREA_ROWS = 3;
+const MIN_TEXTAREA_ROWS = 3
 const content = ref('')
-const textarea = ref();
+const textarea = ref()
 const lines = computed(() => {
   const result = content.value.split('\n')
 
-  return result;
+  result.pop()
+
+  return result
 })
 const aloneEnter = gen_alone_enter_handler(content)
 const onClickFocusTextarea = () => {
@@ -21,23 +20,11 @@ const onClickFocusTextarea = () => {
 
 <template>
   <div class="root">
-    <div
-      @click="onClickFocusTextarea"
-      class="textarea-ghost ghost"
-    >
-      <template
-        v-for="(l, i) of lines"
-        :key="i"
-      >
-        <template
-          v-for="(w, j) of l.split(/\b/)"
-          :key="j"
-        >
-          <pre
-            class='inline'
-            :class="{ tag: !/ +/.test(w) }"
-          >{{ w }}</pre>
-        </template><br />
+    <div @click="onClickFocusTextarea" class="textarea-ghost ghost">
+      <template v-for="(l, i) of lines" :key="i">
+        <template v-for="(w, j) of l.split(/\b/)" :key="j">
+          <pre class="inline" :class="{ tag: !/ +/.test(w) }">{{ w }}</pre></template
+        ><br />
       </template>
     </div>
     <textarea
@@ -45,7 +32,7 @@ const onClickFocusTextarea = () => {
       class="textarea-ghost textarea"
       :value="content"
       @keypress.enter.exact.prevent="aloneEnter"
-      :rows="lines.length > MIN_TEXTAREA_ROWS ? lines.length : MIN_TEXTAREA_ROWS"
+      :rows="(lines.length > MIN_TEXTAREA_ROWS ? lines.length : MIN_TEXTAREA_ROWS) + 1"
       :placeholder="'press Enter to add a new line\nand Shift+Enter to submit the form\n(Shift or Alt or Ctrl or Command...)'"
     ></textarea>
   </div>
--- src/utils/avg.ts
+++ src/utils/avg.ts
@@ -1,8 +1,8 @@
-export function avg(arr: number[], round_type: "ceil" | "floor" = "ceil") {
-  let sum = 0;
+export function avg(arr: number[], round_type: 'ceil' | 'floor' = 'ceil') {
+  let sum = 0
   for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
+    sum += arr[i]
   }
 
-  return Math[round_type](sum / arr.length);
+  return Math[round_type](sum / arr.length)
 }
