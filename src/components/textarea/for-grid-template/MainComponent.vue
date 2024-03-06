d49fd557d2e9d7db0e4c3b0c4cafba76c25daf24
nik-kita <nik-kita.deno.dev>
2024-03-07 00:46:24 +0100

    some progress

--- src/components/textarea/for-grid-template/MainComponent.vue
+++ src/components/textarea/for-grid-template/MainComponent.vue
@@ -6,25 +6,48 @@ import { computed, ref } from 'vue'
 import { gen_alone_enter_handler } from './gen-alone-enter-handler'
 
 const MIN_TEXTAREA_ROWS = 3;
-const content = ref('|_i')
+const content = ref('')
+const textarea = ref();
 const lines = computed(() => {
   const result = content.value.split('\n')
 
   return result;
 })
 const aloneEnter = gen_alone_enter_handler(content)
+const onClickFocusTextarea = () => {
+  textarea.value.focus()
+}
 </script>
 
 <template>
   <div class="root">
+    <div
+      @click="onClickFocusTextarea"
+      class="textarea-ghost ghost"
+    >
+      <template
+        v-for="(l, i) of lines"
+        :key="i"
+      >
+        <template
+          v-for="(w, j) of l.split(/\b/)"
+          :key="j"
+        >
+          <pre
+            class='inline'
+            :class="{ tag: !/ +/.test(w) }"
+          >{{ w }}</pre>
+        </template><br />
+      </template>
+    </div>
     <textarea
+      ref="textarea"
       class="textarea-ghost textarea"
       :value="content"
       @keypress.enter.exact.prevent="aloneEnter"
       :rows="lines.length > MIN_TEXTAREA_ROWS ? lines.length : MIN_TEXTAREA_ROWS"
       :placeholder="'press Enter to add a new line\nand Shift+Enter to submit the form\n(Shift or Alt or Ctrl or Command...)'"
     ></textarea>
-    <div class="textarea-ghost ghost">|_i</div>
   </div>
 </template>
 
