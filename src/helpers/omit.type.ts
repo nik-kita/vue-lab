ce8ca57c68b1df60c2b114867da5cbd1243a7033
nik-kita <nik-kita.deno.dev>
2024-03-08 21:48:18 +0100

    helpers

---
+++ src/helpers/omit.type.ts
@@ -0,0 +1,14 @@
+export type OmitStrict<
+  T extends Record<string, any>,
+  K extends keyof T,
+> = Omit<T, K>;
+
+export type OmitReplace<
+  T extends Record<string, any>,
+  U extends Partial<Record<keyof T, any>>,
+> = Omit<T, keyof U> & U;
+
+export type AddOrReplace<
+  T extends Record<string, any>,
+  U extends Record<string, any>
+> = Omit<T, keyof U> & U;
